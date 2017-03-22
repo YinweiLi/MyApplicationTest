4e815b5496ff92ccff303aa8c1b394477257d9a5
YinweiLi <843056251@qq.com>
2017-03-22 21:31:49 +0800

    实现图片的变化，大小变化和旋转, 多级菜单实现，在注册到textview是出现问题 解决了多级菜单的问题，问题总结在注释里面

--- app/src/main/java/com/example/yinwei/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/yinwei/myapplicationtest/MainActivity.java
@@ -47,11 +47,55 @@ public class MainActivity extends AppCompatActivity {
         Button b = (Button) findViewById(R.id.button);
 
         textView4TestContextMenu = (TextView)findViewById(R.id.textView2);
-        registerForContextMenu(textView4TestContextMenu);
+        //registerForContextMenu(textView4TestContextMenu);
+
         //rigester();
 //        this.registerForContextMenu(myTextView);
         //为什么长按textview我的菜单不出现???????????????
         //Button按钮的点击事件监听器，功能：改变textview里的内容
+        textView4TestContextMenu.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+                //textView4TestContextMenu.setText("what's the F!");
+                menu.setHeaderIcon(R.drawable.image_1);
+                menu.setHeaderTitle("FirstMenu");
+                menu.add(1,0,0,"M1_1");
+                menu.add(1,1,1,"M1_2");
+                menu.add(1,2,2,"M1_3");
+                //上下文菜单之二级菜单/多级菜单的理解
+                SubMenu subMenu1 = menu.addSubMenu(0,3,3,"点我!!");
+                subMenu1.setHeaderIcon(R.drawable.image_1);
+                subMenu1.add(0,0,0,"M2_1");
+                subMenu1.add(0,1,1,"M2_2");
+
+                SubMenu subMenu2 = subMenu1.addSubMenu(0,2,2,"点我!!");
+                subMenu2.add(0,0,0,"M3_1");
+
+                SubMenu subMenu3 = subMenu2.addSubMenu("点我!!");
+                subMenu3.add(0,0,0,"逗你玩!!");
+            }
+/*在学习上下文菜单ContextMenu的时候遇到了很多问题
+  First:利用一个TextView来测试contextMenu的时候，按照书上的内容，
+  在OnCreate()函数中为TextView对象注册上下文菜单，然后再函数外重写OnCreateContextMenu()函数，
+  可是当我长按这个注册了上下文菜单的TextView时，系统并没有自动调用OnCreateContextMenu()函数，
+  就连里面的测试代码(改变textView的text内容代码)都没触发，可见他并没有执行此函数，
+  之后我就想了解一下RegisterForContextMenu()是怎么给控件注册上下文菜单的，当我点进去之后发现，
+  这个注册函数就是给本控件添加了一个上下文菜单时间监听器OnCreateContextMenuListener()，
+  然后我就仿照为控件添加点击事件监听一样的方法来添加上下文菜单创建的监听器，结果监听事件触发了，
+  成功弹出了上下文菜单，但是任然不知道为什么用注册函数RegisterForContextMenu()不好使??????
+  Second:不理解多级菜单的含义，不知道哪个菜单算二级菜单三级菜单，
+  之后再观察代码的过程中，我发现：有一个最初的ContextMenu对象，是系统传进来的，应该属本Activity所有
+  用这个传进来的对象可以创建上下文菜单，但是用add()函数，创建的菜单不能再继续往里面点即不能进入下一级
+  菜单，而用addSubMenu()创建的菜单是进入了新的选择菜单，用他创建的菜单返回的是一个SubMenu对象，而SubMenu类
+  是Menu的子类，也是一个菜单，通过这个方法，就可以创建子菜单，也就是我们所说的多级菜单，最初弹出的是一级菜单
+  在此基础上创建的子菜单点击弹出的就是二级菜单，以此类推，就实现了多级菜单的功能，上下文菜单ContextMenu对象
+  能调用的添加菜单函数只有两个，即add()|addSubMenu(),
+  add()添加的是普通菜单，实现其点击函数来完成功能，
+  addSunMenu()添加的是子菜单，也就是多级菜单，实现多层功能的选择
+
+
+  */
+        });
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -287,13 +331,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnCreateContextMenu(ContextMenu contextMenu, View view, ContextMenuInfo contextMenuInfo){
-        super.onCreateContextMenu(contextMenu,view,contextMenuInfo);
+        //super.onCreateContextMenu(contextMenu,view,contextMenuInfo);
+        textView4TestContextMenu.setText("chcked1");
         if(view == textView4TestContextMenu){
-            contextMenu.setHeaderIcon(R.drawable.image_1);
-            contextMenu.setHeaderTitle("FirstMenu");
-            contextMenu.add(1,0,0,"M1");
-            contextMenu.add(1,1,1,"M2");
-            contextMenu.add(1,2,2,"M3");
+            textView4TestContextMenu.setText("chcked");
+//            contextMenu.setHeaderIcon(R.drawable.image_1);
+//            contextMenu.setHeaderTitle("FirstMenu");
+//            contextMenu.add(1,0,0,"M1");
+//            contextMenu.add(1,1,1,"M2");
+//            contextMenu.add(1,2,2,"M3");
 
 //            SubMenu subMenu2 = contextMenu.addSubMenu("Menu2");
 //            subMenu2.setHeaderIcon(R.drawable.image_1);
